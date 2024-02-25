eabbfa969c2272d205ab9e0827ffb1f211e48335
lunanarae <lunanarae@gmail.com>
2024-02-25 20:20:38 +0800

    updated javascript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,58 @@
+function currentWeatherDetails(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#location");
+  let weatherCondition = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("icon");
+
+  console.log(response.data);
+  weatherCondition.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  cityElement.innerHTML = response.date.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+function searchCity(city) {
+  let apiKey = "033b43a04493c2b0f53e8fe8bdote92d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherDetails);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    let minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    let hours = `0${hours}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
 function search(event) {
   event.preventDefault();
   let searchFormInputElement = document.querySelector("#search-form-input");
+  searchCity(searchFormInputElement.value);
+
   let locationElement = document.querySelector("#location");
 
   locationElement.innerHTML = searchFormInputElement.value;
@@ -9,6 +61,4 @@ function search(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
 
-let apiKey = "033b43a04493c2b0f53e8fe8bdote92d";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${location}&key={apiKey}&unit=metric";
+searchCity("Perth");
