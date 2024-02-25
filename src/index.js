432e0bcc1a60f92b4218f99c2d3c7be9fe707831
lunanarae <lunanarae@gmail.com>
2024-02-25 08:48:41 +0800

    add javascript function search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function search(event) {
+  event.preventDefault();
+  let searchFormInputElement = document.querySelector("#search-form-input");
+  let locationElement = document.querySelector("#location");
 
+  locationElement.innerHTML = searchFormInputElement.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", search);
